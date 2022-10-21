560c82264d491129ff6665d0191391a2a6ce9bf3
Joshua Morony <joshua.morony@gmail.com>
2022-10-21 10:44:28 +1030

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'lucky';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/').subscribe((res) => {
+      console.log(res);
+    });
+
+    const guess = { guess: 14 };
+
+    this.http.post('http://localhost:3000/', guess).subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
